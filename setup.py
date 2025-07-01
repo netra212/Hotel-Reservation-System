e3b1d5a0b89e0acb2b9cdf157db131d2eca89a69
netra212 <netra200021kcbdr@gmail.com>
2025-07-01 09:33:26 +0100

    Adding setup files

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 # This File is used for the project management. 
+from setuptools import setup,find_packages
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+    
+setup(
+    name = "hotel reservation system",
+    version = "1.0.0",
+    description = "This project will classify either the booking will be cancel or not.", 
+    author = "Netra Khatri",
+    packages = find_packages(),
+    install_requires = requirements
+)
